a7fc77c5b0f0327de9fd607c44030df19c78abbd
shohei-saito07 <naposuke7@gmail.com>
2024-04-05 02:28:57 +0900

    カテゴリのSeederを修正

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\MajorCategory;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -33,31 +34,44 @@ class CategoriesTableSeeder extends Seeder
 
         foreach ($major_category_names as $major_category_name) {
             if ($major_category_name == '和食') {
+                MajorCategory::create([
+                    'name' => $major_category_name,
+                    'description' => $major_category_name,
+                ]);
+
                 foreach ($japanese_food_categories as $japanese_food_category) {
                     Category::create([
                         'name' => $japanese_food_category,
                         'description' => $japanese_food_category,
-                        'major_category_name' => $major_category_name
+                        'major_category_id' => '1'
                     ]);
                 }
             }
 
             if ($major_category_name == '洋食') {
+                MajorCategory::create([
+                    'name' => $major_category_name,
+                    'description' => $major_category_name,
+                ]);
                 foreach ($western_food_categories as $western_food_category) {
                     Category::create([
                         'name' => $western_food_category,
                         'description' => $western_food_category,
-                        'major_category_name' => $major_category_name
+                        'major_category_id' => '2'
                     ]);
                 }
             }
 
             if ($major_category_name == 'カレー') {
+                MajorCategory::create([
+                    'name' => $major_category_name,
+                    'description' => $major_category_name,
+                ]);
                 foreach ($curry_categories as $curry_category) {
                     Category::create([
                         'name' => $curry_category,
                         'description' => $curry_category,
-                        'major_category_name' => $major_category_name
+                        'major_category_id' => '3'
                     ]);
                 }
             }
