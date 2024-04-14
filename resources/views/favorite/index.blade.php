b413a9cee3630e9f93569895eef224c854b44bf5
shohei-saito07 <naposuke7@gmail.com>
2024-04-15 02:08:01 +0900

    deploy to heroku

--- resources/views/favorite/index.blade.php
+++ resources/views/favorite/index.blade.php
@@ -20,31 +20,34 @@
             <div class="col-md-2">
                 <p>お気に入り解除</p>
             </div>
-            @foreach ($favorite_stores as $favorite_store)
-            <div class="col-md-2 mt-2">
-                <a href="{{route('stores.show', $favorite_store->id)}}">
-                    <img src="{{ asset('img/dummy.png')}}" class="img-fluid w-100">
-                </a>
-            </div>
-            <div class="col-md-4 mt-4">
-                <h3 class="mt-4">{{$favorite_store->name}}</h3>
-            </div>
-            <div class="col-md-4">
-                <h3 class="w-100 mt-4">{{$favorite_store->description}}</h3>
-            </div>
-            <div class="col-md-2">
-                <h3 class="w-100 mt-4"></h3>
-                <a href="{{ route('favorites.destroy', $favorite_store->id) }}" class="btn eating-log-favorite-button text-favorite w-100" onclick="event.preventDefault(); document.getElementById('favorites-destroy-form').submit();">
-                    <i class="fa fa-heart"></i>
-                    お気に入り解除
-                </a>
-            </div>
-            @endforeach
+            @if($favorite_stores ==! null OR !empty($favorite_stores))
+                @foreach ($favorite_stores as $favorite_store)
+                <div class="col-md-2 mt-2">
+                    <a href="{{route('stores.show', $favorite_store->id)}}">
+                        <img src="{{ asset('img/dummy.png')}}" class="img-fluid w-100">
+                    </a>
+                </div>
+                <div class="col-md-4 mt-4">
+                    <h3 class="mt-4">{{$favorite_store->name}}</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3 class="w-100 mt-4">{{$favorite_store->description}}</h3>
+                </div>
+                <div class="col-md-2">
+                    <h3 class="w-100 mt-4"></h3>
+                    <a href="{{ route('favorites.destroy', $favorite_store->id) }}" class="btn eating-log-favorite-button text-favorite w-100" onclick="event.preventDefault(); document.getElementById('favorites-destroy-form').submit();">
+                        <i class="fa fa-heart"></i>
+                        お気に入り解除
+                    </a>
+                </div>
+                <form id="favorites-destroy-form" action="{{ route('favorites.destroy', $favorite_store->id) }}" method="POST" class="d-none">
+                        @csrf
+                        @method('DELETE')
+                </form>
+                @endforeach
+            @endif
         </div>
-        <form id="favorites-destroy-form" action="{{ route('favorites.destroy', $favorite_store->id) }}" method="POST" class="d-none">
-                @csrf
-                @method('DELETE')
-        </form>
+        
         <hr>
     </div>
 </div>
